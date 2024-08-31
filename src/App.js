1325592c98ff5749b2f97b2f4a7adc13034bcd03
user <a0903131602@gmail.com>
2024-08-31 13:59:27 +0800

    加入環境變數

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const ApiUrl = process.env.REACT_APP_API_URL;
+const ApiPath = process.env.REACT_APP_API_PATH;
+
+const App = () => {
+  useEffect(()=>{
+   (async()=>{
+     const res = await axios.get(`/v2/api/${ApiPath}/products/all`)
+     console.log(res)
+   })();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        123
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
